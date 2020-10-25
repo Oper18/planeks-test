a02322ae01071f75efbd6a3f1a57cc2cc3e8b2a3
Grigory <gnovikov93@gmail.com>
2020-10-25 16:50:17 +0300

    correct wsgi

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0:8000 --access-logfile - --error-logfile - planeks.wsgi &
+gunicorn -b 0.0.0.0:8000 --access-logfile - --error-logfile - planeks_test.wsgi &
 celery -A planeks_test worker -l info -B
